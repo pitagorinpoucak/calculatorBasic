674e622d26ac3bf1d429664260a897ffd2d379f6
Luka <podvrsina@gmail.com>
2022-05-29 11:33:59 +0200

    - started basic functionality again

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,12 @@ const numbers = Array.from(document.querySelectorAll(".number"));
 const operators = Array.from(document.querySelectorAll(".operator"));
 
 const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const operatorArray = ["+", "-", "*", "/", ".", ",", "C", "c", "=", "Enter"];
+const operatorArray = ["+", "-", "*", "/"];
+const functionArray = [".", ",", "C", "c", "=", "Enter"];
+
+let operatorOne = null;
+let operatorTwo = null;
+let operation = "";
 
 window.addEventListener("keydown", (e) => {
   if (e.key === "Backspace") {
@@ -13,4 +18,45 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
-window.addEventListener("keypress", (e) => console.log(e.key));
+window.addEventListener("keypress", (e) => keyPress(e.key));
+
+function keyPress(key) {
+  if (numberArray.includes(Number(key))) {
+    display.textContent += key;
+  }
+
+  if (operatorArray.includes(key)) {
+    if (operatorOne === null) {
+      operatorOne = Number(display.textContent);
+      operation = key;
+      display.textContent = "";
+      return;
+    }
+    operatorTwo = Number(display.textContent);
+    display.textContent = "";
+  }
+
+  if (functionArray.includes(key)) {
+    switch (key) {
+      case ".":
+      case ",":
+        break;
+      case "C":
+      case "c":
+      case "AC":
+        break;
+      case "=":
+      case "Enter":
+        enterPressed();
+        break;
+    }
+  }
+
+  console.log(operatorOne, operation, operatorTwo);
+}
+
+function enterPressed() {
+  if (operatorOne === null || operatorTwo === null || operation === "") {
+    return;
+  }
+}
